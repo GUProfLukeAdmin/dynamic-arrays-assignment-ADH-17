4ead669783adefd11d45cbc09d4606f527dcf9f7
ADH-17 <ayden.humphries17@gmail.com>
2025-02-11 11:25:14 -0800

    fixed the part 2 to show that the bits are stored in the same byte.

--- vector.cpp
+++ vector.cpp
@@ -12,7 +12,7 @@ void part1() {
         
     }
     // log the size and capacity of the vector
-    cout << "After Adding Elements: Size = " << vec.size() << ", Capacity = " << vec.capacity() << endl;
+    cout << "After Adding Element s: Size = " << vec.size() << ", Capacity = " << vec.capacity() << endl;
 
     //Remove half of the elements of the vector
     for(int i = 50000; i > 2500; i--){
@@ -28,12 +28,11 @@ void part1() {
 void part2() {
     //TODO: Implement part 2
     // a boolean vector and char vector both with 10000 elements 
-    vector<bool> vec_bool(10000); 
-    vector<char> vec_char(10000);
+    vector<bool> vec_bool(10000, true); 
+    vector<char> vec_char(10000, 'a');
 
-
-    cout << "Memory usage of vector<bool>: " << vec_bool.capacity() << " bytes" << endl;
-    cout << "Memory usage of vector<char>: " << vec_char.capacity() << " bytes" << endl;
+    cout << "Memory usage of vector<bool> = " << vec_bool.size()/8 << " due to bit condensing" << endl;
+    cout << "Memory usage of vector<char> = " << vec_char.size() << " normal sizing" << endl;
 }
 
 int main() {
